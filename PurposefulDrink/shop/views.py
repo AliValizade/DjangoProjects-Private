102fa4ed57042d7cc8cbcd6ceb48d52a9803a911
AliValizade <alivalizade@gmail.com>
2024-04-26 01:39:48 +0330

    Create recommend_herb view

--- PurposefulDrink/shop/views.py
+++ PurposefulDrink/shop/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from .models import Herb, Disease, Suitability, NeutralPackage
 
-# Create your views here.
+from .forms import DiseaseForm
+
+def recommend_herbs(request):
+    if request.method == 'POST':
+        form = DiseaseForm(request.POST)
+        if form.is_valid():
+            selected_diseases = form.cleaned_data['diseases']
+            herbs = Herb.objects.all()
+            herb_scores = {herb: herb.get_total_score(selected_diseases) for herb in herbs}
+            forbidden_herbs = {herb for herb in herbs if herb.is_forbidden(selected_diseases)}
+
+            # Delete forbidden herbs from recommendations
+            for herb in forbidden_herbs:
+                herb_scores.pop(herb, None)
+
+            # Sort herbs by score and select top 3
+            recommended_herbs = sorted(herb_scores, key=herb_scores.get, reverse=True)[:3]
+            recommendations = [(herb.name, herb_scores[herb]) for herb in recommended_herbs]
+            neutral_packages = NeutralPackage.objects.all()
+
+            # Create a list of descriptions for forbidden herbs
+            forbidden_list = [(herb.name, ', '.join([disease.name for disease in selected_diseases if herb.is_forbidden([disease])])) for herb in forbidden_herbs]
+
+            return render(request, 'shop/recommend.html', {
+                'recommendations': recommendations,
+                'neutral_packages': neutral_packages,
+                'forbidden_list': forbidden_list
+            })
+    else:
+        form = DiseaseForm()
+
+    return render(request, 'shop/index.html', {'form': form})
